f8fc7d00a298025c03a5b690653a465f1a441be7
robsu17 <robson.costa134@gmail.com>
2025-06-10 11:25:29 -0300

    Add StatusIcon component and update Filter to use it; refactor styles

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,7 +1,7 @@
 import { FilterStatus } from '@/types/FilterStatus'
 import { TouchableOpacity, TouchableOpacityProps, Text } from 'react-native'
 import { styles } from './styles'
-import { CircleCheck,  } from 'lucide-react-native'
+import { StatusIcon } from '../StatusIcon'
 
 interface FilterProps extends Omit<TouchableOpacityProps, 'style'> {
     status: FilterStatus
@@ -15,7 +15,7 @@ export function Filter({ status, isActive, ...rest }: FilterProps) {
             activeOpacity={0.8}
             {...rest}
         >
-            <CircleCheck size={18} color="#000" />
+            <StatusIcon status={status} />
 
             <Text style={styles.title}>
                 { status === FilterStatus.DONE ? 'Comprados' : 'Pendentes' }
