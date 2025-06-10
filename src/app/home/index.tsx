626caefa091b83af3d7ed83cf8a296e9ecca4de4
robsu17 <robson.costa134@gmail.com>
2025-06-10 10:58:10 -0300

    Add Filter component with styles and integrate into Home; define FilterStatus type

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -2,6 +2,8 @@ import { Image, View } from 'react-native';
 import { styles } from './styles';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
+import { Filter } from '@/components/Filter';
+import { FilterStatus } from '@/types/FilterStatus';
 
 export default function Home() {
 	return (
@@ -14,7 +16,8 @@ export default function Home() {
 			</View>
 
 			<View style={styles.content}>
-
+				<Filter status={FilterStatus.DONE} isActive />
+				<Filter status={FilterStatus.PENDING} isActive={false} />
 			</View>
 		</View>
 	);
---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,22 @@
+import { FilterStatus } from '@/types/FilterStatus'
+import { TouchableOpacity, TouchableOpacityProps, Text } from 'react-native'
+import { styles } from './styles'
+
+interface FilterProps extends Omit<TouchableOpacityProps, 'style'> {
+    status: FilterStatus
+    isActive: boolean
+}
+
+export function Filter({ status, isActive, ...rest }: FilterProps) {
+    return (
+        <TouchableOpacity 
+            style={[styles.container, { opacity: isActive ? 1 : 0.5 }]} 
+            activeOpacity={0.8}
+            {...rest}
+        >
+            <Text style={styles.title}>
+                { status === FilterStatus.DONE ? 'Comprados' : 'Pendentes' }
+            </Text>
+        </TouchableOpacity>
+    )
+}
