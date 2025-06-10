30847742d7d540fb20a54bc765a9b0221c8c3db2
robsu17 <robson.costa134@gmail.com>
2025-06-10 08:07:20 -0300

    Refactor app structure: replace App component with Home, add logo assets, and include splash icon

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
+import Home from './src/app/home';
 
-import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Home);
