3170c5ce563614aaf5eb94e120b9f8feff5c035d
robsu17 <robson.costa134@gmail.com>
2025-06-11 11:58:25 -0300

    Add Item component with styles and integrate into Home; implement FlatList for item rendering

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,16 +1,31 @@
-import { Image, TouchableOpacity, View, Text } from 'react-native';
+import { Image, TouchableOpacity, View, Text, ScrollView, FlatList } from 'react-native';
 import { styles } from './styles';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import { Filter } from '@/components/Filter';
 import { FilterStatus } from '@/types/FilterStatus';
+import { Item, ItemData } from '@/components/Item';
 
 const FILTER_STATUS: FilterStatus[] = [
 	FilterStatus.DONE,
 	FilterStatus.PENDING,
 ];
 
+const ITEMS: ItemData[] = [
+	{ status: FilterStatus.DONE, description: "1 pacote de café" },
+	{ status: FilterStatus.PENDING, description: "3 pacotes de macarrão" },
+	{ status: FilterStatus.PENDING, description: "3 cebolas" },
+]
+
 export default function Home() {
+	const handleRemove = () => {
+		console.log("Remover item")
+	}
+
+	const handleStatus = () => {
+		console.log("Muda status")
+	}
+
 	return (
 		<View style={styles.container}>
 			<Image source={require('@/assets/logo.png')} style={styles.logo} />
@@ -31,6 +46,23 @@ export default function Home() {
 						<Text style={styles.clearText}>Limpar</Text>
 					</TouchableOpacity>
 				</View>
+
+				<FlatList
+					showsVerticalScrollIndicator={false}
+					showsHorizontalScrollIndicator={false}
+					contentContainerStyle={styles.listContent}
+					data={ITEMS}
+					ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
+					ItemSeparatorComponent={() => <View style={styles.separator} />}
+					renderItem={({ item, index }) => (
+						<Item
+							key={index}
+							onRemove={handleRemove}
+							onStatus={handleStatus}
+							data={item}
+						/>
+					)}
+				/>
 			</View>
 		</View>
 	);
